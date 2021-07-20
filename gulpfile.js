404454a90135e82c6b4cc03eebb4ff3919b5973e
Nunifuchisaka <nunifuchisaka@gmail.com>
2021-07-20 09:59:44 +0900

    carousel

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    browserSync = require('browser-sync').create(),
+    sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps');
+
+var path = {
+  src: 'src',
+  dst: 'htdocs'
+};
+
+
+/*
+## browser sync
+*/
+
+gulp.task('bs-reload', function(done){
+  browserSync.reload();
+  done();
+});
+
+
+/*
+## StyleSheet
+*/
+
+gulp.task('sass', function(){
+  return gulp.src(path.src + '/scss/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer(),
+    ]))
+    .pipe(gulp.dest(path.dst + '/assets/css'));
+});
+
+
+/*
+## watch
+*/
+
+gulp.task('default', function(done){
+  
+  browserSync.init({
+    server: {
+      baseDir: path.dst
+    }
+  });
+  
+  gulp.watch(path.dst + '/**/*.html', gulp.task('bs-reload'));
+  gulp.watch(path.dst + '/**/*.php', gulp.task('bs-reload'));
+  gulp.watch(path.dst + '/**/*.css', gulp.task('bs-reload'));
+  gulp.watch(path.dst + '/**/*.js', gulp.task('bs-reload'));
+  
+  gulp.watch(path.src + '/scss/**/*.scss', gulp.series('sass'));
+  
+  done();
+});
